cc6290d62c3ea8d547cb0b53cb44a2a8c2c62951
tammy vu <tammy.vsk@gmail.com>
2023-02-20 12:36:19 -0600

    asdggsadg

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
-  
+  public static int[][] copySquare(int[][] arr) {
+    int[][] copyOfSqr = new int [][];
+    int numOfRows = arr.length-1;
+    for(int r = 0; r < numOfRows; r++){
+      for(int c = 0; c < arr[0].length; c++){
+
+      }
+    }
+  }
   public static void main(String[] args) {
     
   }
