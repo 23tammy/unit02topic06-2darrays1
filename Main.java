692ac41b09bcdbc0a8dd6738e7de3c4acc3f2456
Daniel Scislowski <mr.scislowski@gmail.com>
2023-02-23 09:52:44 -0600

    feedback

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class Main {
+  // good
   public static int[][] copySquare(int[][] arr) {
     int side = arr.length;
     int[][] copyOfSqr = new int[side][side];
@@ -12,6 +13,7 @@ public class Main {
     return copyOfSqr;
   }
 
+  // good
   public static int[][] copyRectangle(int[][] arr) {
     int[][] copyOfRec = new int[arr.length][arr[0].length];
     for(int r = 0; r < arr.length; r++){
@@ -25,6 +27,7 @@ public class Main {
   public static int[][] copyRagged(int[][] arr) {
     int[][] copyOfRag = new int [arr.length][];
     for(int r = 0; r < arr.length; r++){{ // copy row; add to array
+      // FIXME: this doesn't actually create a copy. See my demo in main.
       copyOfRag[r] = arr[r];
         }
       }
@@ -78,33 +81,40 @@ public class Main {
     return newSpread;
   }
   public static void main(String[] args) {
-    int[][] arr1 = new int[][]{
-      {1, 0, 8},
-      {4, 5, 5},
-      {10, 11, 12}};
-    int[][] arr2 = new int[][]{
-      {1, 0, 6},
-      {4, 5, 5},
-      {7, 4, 5},
-      {10, 11, 12}};
+    // int[][] arr1 = new int[][]{
+    //   {1, 0, 8},
+    //   {4, 5, 5},
+    //   {10, 11, 12}};
+    // int[][] arr2 = new int[][]{
+    //   {1, 0, 6},
+    //   {4, 5, 5},
+    //   {7, 4, 5},
+    //   {10, 11, 12}};
     int[][] arr3 = new int[][]{
       {1, 0},
       {4, 5, 5},
       {7,},
       {10, 11, 12}};
-    double[][] arr4 = new double[][] {
-      {99, 85, 98},
-      {98, 57, 79},
-      {92, 77, 74},
-      {94, 62, 81},
-      {99, 94, 92},
-      {80, 76.5, 67},
-      {76, 58.5, 90.5},
-      {92, 66, 91},
-      {77, 70.5, 66.5},
-      {89, 89.5, 81}};
 
-      double[] weights = { 0.25, 0.25, 0.50 };
+    int[][] copyOfarr3 = copyRagged(arr3);
+    // changing values in the copy shouldn't affect the original array
+    copyOfarr3[0][0] = 9999;
+    // but it does. Because it's not technically a true copy
+    System.out.println(Arrays.deepToString(arr3));
+
+    // double[][] arr4 = new double[][] {
+    //   {99, 85, 98},
+    //   {98, 57, 79},
+    //   {92, 77, 74},
+    //   {94, 62, 81},
+    //   {99, 94, 92},
+    //   {80, 76.5, 67},
+    //   {76, 58.5, 90.5},
+    //   {92, 66, 91},
+    //   {77, 70.5, 66.5},
+    //   {89, 89.5, 81}};
+
+    //   double[] weights = { 0.25, 0.25, 0.50 };
 
       //System.out.println(Arrays.deepToString(copySquare(arr1)));
       //System.out.println(Arrays.deepToString(copyRectangle(arr2)));
